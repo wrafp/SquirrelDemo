0a0b777094e41ed0007c6a58bd041a1c2180899f
Kevin Huang <kevin.huang23@ycdsbk12.ca>
2022-10-19 10:55:36 -0400

    Squirrel demo complete

--- src/OOPDemo/SquirrelDemo.java
+++ src/OOPDemo/SquirrelDemo.java
@@ -1,5 +1,31 @@
 package OOPDemo;
 
+import OOPDemo.*;
+
 public class SquirrelDemo {
-    
+    public static void main(String[] args) {
+        
+        // Define squirrel objects
+        Squirrel alvin;
+        Squirrel simon;
+        Squirrel theodore;
+        
+        // Create instances (constructor called)
+        alvin = new Squirrel("Red", "Medium", 8, 100.0);
+        simon = new Squirrel("Blue", "Tall", 2, 240.5);
+        theodore = new Squirrel("Green", "Grande", 15, 66.6);
+
+        // Method calls
+        alvin.climb();
+        alvin.run();
+        simon.findNut(3);
+        theodore.eatNut();
+        theodore.findNut(23);
+        simon.run();
+        System.out.println("Theodore's nut count: " + theodore.getNutCount());
+        simon.kill();
+
+        System.out.println("Simon's IQ: " + simon.iqDouble);
+
+    }
 }
